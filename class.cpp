b85347ade7baf0278b94aaa2e9360f53dfcca868
Alex-S-123 <alex_saltykov06@mail.ru>
2025-02-17 18:06:28 +0300

    Add files via upload

--- class.cpp
+++ class.cpp
@@ -6,42 +6,78 @@ class object{
 protected:
     float x, y; //coords
     int stx, sty; //size of texture
-    //Texture tex;
+    //Sprite s; ?
 public:
-    object();
+    object(/*Texture te*/){
+        this->x = 0;
+        this->y = 0;
+        this->stx = 0;
+        this->sty = 0;
+        //this->st.setTexture(te);?
+    }
 
-    void set_pos(float x0, float y0);
+    void set_pos(float x0, float y0){
+        this->x = x0;
+        this->y = y0;
+    }
 
-    void set_tex_size(int x0, int y0);
+    void set_tex_size(int x0, int y0){
+        this->stx = x0;
+        this->sty = y0;
+    }
 
-    float get_x();
+    float get_x() {return this->x;}
 
-    float get_y();
+    float get_y() {return this->y;}
 
-    float get_tx();
+    int get_tx() {return this->stx;}
 
-    float get_ty();
+    int get_ty() {return this->sty;}
 
-    void set_texture();
+    void set_texture(int x0, int y0){
+        //this->st.setTextureRect();?
+    }
+
+    void draw(){
+    //this->st.draw();?
+    }
 
 
 };
-object::object(){
-    this->x = 0;
-    this->y = 0;
-    this->stx = 0;
-    this->sty = 0;
-}
 
-void object::set_pos(float x0, float y0) {
-    this->x = x0;
-    this->y = y0;
-}
+class item{
+protected:
+    int boost_damage, boost_armor, boost_hp, use_before_break, time_of_destruction, weight;
+    bool is_active;
+    int x0, y0, x1, y1; //texture rect for menue
+    char* name;
+public:
+
+};
+
+class monster: public object{
+protected:
+    int hp, armor, damage, speed;
+public:
+    void damaged(int damag);
+    int atack();
+    virtual void behavior();
+};
+
+class player: public monster{
+protected:
+    item* inner;
+    int type;
+    int weight;
+public:
+    action();
+};
 
 
 class treasure: public object{
 protected:
-    int* inner;
+    int n;
+    item* inner;
 public:
     treasure();
     bool give(int type, int number);
@@ -59,16 +95,12 @@ protected:
     int type;
     bool destr_key;
 public:
-    bool try_to_open(Player p);
+    bool try_to_open(player p);
 };
 
-class chest:public treasure{
-protected:
-    bool is_open;
-    int type;
-    bool destr_key;
-public:
-    bool try_to_open(Player p);
+class chest:public treasure, public gate{
+    bool give(int type, int number);
+    bool take(int type, int number);
 };
 
 class teleport: gate{
@@ -91,23 +123,6 @@ public:
     bool sell();
 };
 
-class monster: public object{
-protected:
-    int hp, armor, damage, speed;
-public:
-    damaged(int damag);
-    atack(monster* m);
-    behavior();
-};
-
-class player: public monster{
-protected:
-    int* inner;
-    int money;
-    int type;
-public:
-    action();
-};
 
 class soyjak_typical: public monster {
 protected:
@@ -126,11 +141,11 @@ public:
         this->walking_radius = 10;
         this->attack_radius = 5;
         this->right_direction = true;
-    };
+    }
 
     ~soyjak_typical() {
         delete this;
-    };
+    }
 
     void behavior() {
         // walking (now exists only on x-axis)
@@ -148,8 +163,16 @@ public:
             }
         }
         // attack (attacks only on x-axis)
-        if (pow(pow(player.get_x() - this->x, 2) + pow(player.get_y() - this->y, 2), 1/2) {
+        //if (pow(pow(player.get_x() - this->x, 2) + pow(player.get_y() - this->y, 2), 1/2)) {
             // change of image to attacking image
-        /
-    };
+        //}
+    }
 };
+
+
+
+
+
+
+
+
