fb2bbf5d88c46e17873030d785920971df682d17
YuriyKarabintsev777 <karabintsev.iuv@phystech.edu>
2025-03-10 21:33:36 +0700

    Add files via upload

--- class.cpp
+++ class.cpp
@@ -6,9 +6,9 @@ using namespace sf;
 class object{
 protected:
     int x, y; //coords
-    int x0, y0, x1, y1; //texture rect
+    int x0, y0, x1, y1; //texture rect 
 public:
-    object(int sx, int sy, int tx0, int ty0) {
+    object(int sx, int sy, int tx0, int ty0){
         this->x = 0;
         this->y = 0;
         this->x1 = sx;
@@ -44,7 +44,19 @@ class effect: public item{
     virtual void use(/*monster* m*/); //i dont know how to do it
 };
 
-//here was monster
+class monster: public object{
+protected:
+    int hp, armor, damage, speed;
+    int type;
+    effect* effects;    //because we have this
+    item* inner;
+public:
+    monster();
+    void damaged(int damag);
+    int atack();
+    int get_type(){return this->type;}
+    virtual void behavior() = 0;
+};
 
 class player: public object{
 protected:
@@ -53,7 +65,7 @@ protected:
     effect* effects;    //because we have this
     item* inner;
 public:
-    player(int sx, int sy, int tx0, int ty0) : object(sx, sy, tx0, ty0) {
+    player(int sx, int sy, int tx0, int ty0) : object(sx, sy, tx0, ty0){
     }
     void action(){
 	if(Keyboard::isKeyPressed(Keyboard::W)) x -= 1;
@@ -114,31 +126,16 @@ public:
     bool sell();
 };
 
-// HERE ARE ALL MONSTERS.
-
-class monster: public object{
-protected:
-    int hp, armor, damage, speed;
-    int type;
-    effect* effects;    //because we have this
-    item* inner;
-public:
-    monster(int sx, int sy, int tx0, int ty0) : object(sx, sy, tx0, ty0) {}
-    void damaged(int damag);
-    int atack();
-    int get_type(){return this->type;}
-    virtual void behavior() = 0;
-};
 
 class soyjak_typical: public monster {
 protected:
     int xp, yp; // monster walks around this point
-    int walking_radius; // max radius from the point (xp; yp)
+    int walking_radius; // max radius from the point (x0; y0)
     int attack_radius; // if player is closer than this radius, monster attacks
     bool right_direction;
     //need texture size!
 public:
-    soyjak_typical(int sx, int sy, int tx0, int ty0, int xp, int yp) : monster(sx, sy, tx0, ty0) {
+    soyjak_typical(float x0, float y0) {
         this->hp = 6;
         this->armor = 3;
         this->damage = 2;
@@ -148,7 +145,7 @@ public:
         this->walking_radius = 10;
         this->attack_radius = 5;
         this->right_direction = true;
-    } // constructor fixed
+    }
 
     ~soyjak_typical() {
         delete[] inner;
@@ -158,14 +155,14 @@ public:
     void behavior() {
         // walking (now exists only on x-axis)
         if (this->right_direction) {
-            if (this->y <= this->yp + walking_radius) {
-                this->y += this->speed;
+            if (this->x <= this->x0 + walking_radius) {
+                this->x += this->speed;
             } else {
                 this->right_direction = false;
             }
         } else {
-            if (this->y >= this->yp - walking_radius) {
-                this->y -= this->speed;
+            if (this->x >= this->x0 - walking_radius) {
+                this->x -= this->speed;
             } else {
                 this->right_direction = true;
             }
@@ -174,63 +171,6 @@ public:
         //if (pow(pow(player.get_x() - this->x, 2) + pow(player.get_y() - this->y, 2), 1/2)) {
             // change of image to attacking image
         //}
-    }
-};
-
-class bomb: public monster {
-// bomb falls out of flight periodically
-// if bomb falls on player, player is damaged
-protected:
-public:
-    bomb(int sx, int sy, int tx0, int ty0, int speed) : monster(sx, sy, tx0, ty0) {
-    }
-    ~bomb() {}
-    void behavior() {
-        this->set_pos(x - speed, y);
-        // if bomb contacts ground, it is destroyed! It hasn't been created yet.
-    }
-};
-class flight: public monster {
-// monster flies up and down
-// monster throws bombs
-// monster falls if it dies
-protected:
-    int xp, yp;
-    int fly_radius;
-    bool up_direction;
-    int period_bomb;
-    int time_last;
-public:
-    flight(int sx, int sy, int tx0, int ty0, int xp, int yp, int period) : monster(sx, sy, tx0, ty0) {
-        this->period_bomb = period;
-        this->xp = xp;
-        this->yp = yp;
-        this->period_bomb = period;
-        this->time_last = 0;
-        this->fly_radius = 10;
-        this->speed = 8;
-        this->up_direction = true;
-    }
-    void behavior() {
-        // flying (now exists only on x-axis)
-        if (this->up_direction) {
-            if (this->x <= this->xp + this->fly_radius) {
-                this->x += this->speed;
-            } else {
-                this->up_direction = false;
-            }
-        } else {
-            if (this->x >= this->x0 - up_direction) {
-                this->x -= this->speed;
-            } else {
-                this->up_direction = true;
-            }
-        }
-        // attack (attacks only on x-axis)
-        if (clock() - this->time_last > this->period_bomb) {
-            this->time_last = 0;
-            // bomb is created
-        }
     }
 };
 
@@ -268,13 +208,12 @@ int main(){
         {
         //all actions of player, monsters, and others
 		player_1->action();
-		// here will be behaviors of monsters
 		if (event.type == Event::Closed) window.close();
         }
 
     window.clear(Color(192, 192, 192));
-            for(int j = 0; j < n; j++){
-            for(int i = 0; i < n; i++){
+            for(int j = 0; j < 15; j++){
+            for(int i = 0; i < 10; i++){
                 if(abs(player_1->get_x()-30*i)+abs(player_1->get_y() - 30*j) < 41*30 && level[i][j] != 0){
                     field.setPosition(30-player_1->get_y()+30*i, 30 - player_1->get_x() + 30*j);
                     window.draw(field);
@@ -286,4 +225,13 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+
 
